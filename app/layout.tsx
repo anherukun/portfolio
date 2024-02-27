be7b8c2c11d2d34a2a53841077c7257fa8afe519
Angel Gerardo Jimenez Reyes <angel.g.j.reyes@gmail.com>
2024-02-26 18:40:42 -0600

    activacion de vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/react';
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 
@@ -25,6 +26,7 @@ export default function RootLayout({
 	return (
 		<html lang="es">
 			<body className={poppins.className}>{children}</body>
+			<Analytics />
 		</html>
 	);
 }
